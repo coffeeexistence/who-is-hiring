6701262912e6ca9cb04cb59ac9b8722a36ea255a
John Anthony Rivera <askinganthony@gmail.com>
2016-06-02 21:07:12 -0700

    Working awesome now!

--- src/app/features/posting-searcher/HNApi.service.js
+++ src/app/features/posting-searcher/HNApi.service.js
@@ -1,8 +1,23 @@
 function HNApi($http){
   var service = this;
 
+  var pendingRequests = [];
+
   service.showItem = function (id) {
-    return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json')
+    //var canceler = $q.defer();
+    //var promise = $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json', {timeout: canceler.promise});
+    //pendingRequests.push(canceler);
+    //return promise.then(function(res) {
+
+    //  return res;
+    //});
+    //return promise;
+
+    return $http.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json');
+  };
+
+  service.showUser = function (id) {
+    return $http.get('https://hacker-news.firebaseio.com/v0/user/' + id + '.json');
   };
 
 };
--- src/app/features/posting-searcher/whoIsHiring.directive.js
+++ src/app/features/posting-searcher/whoIsHiring.directive.js
@@ -1,9 +1,41 @@
 function whoIsHiring(HNApi) {
 	return {
 		restrict: 'E',
-		controller: ['$scope', '$sce', '$filter',
-		function($scope, $sce, $filter){
+		controller: ['$scope', '$sce', '$filter', '$mdSidenav', '$mdMedia',
+		function($scope, $sce, $filter, $mdSidenav, $mdMedia){
 			var ctrl =  this;
+
+			$scope.$mdMedia = $mdMedia;
+
+			$scope.openLeftMenu = function() {
+		    $mdSidenav('left').toggle();
+		  };
+
+			var submissionLimit = 12;
+
+			$scope.submissions = [];
+			$scope.filteredSubmissions = [];
+
+			var filterSubmissions = function() {
+				var searchTerm = "hiring?"
+				$scope.filteredSubmissions = $filter('filter')($scope.submissions, searchTerm);
+			};
+
+			var getSubmissions = function(ids) {
+				ids.forEach(function(id, i, ids){
+					HNApi.showItem(id).then(function(res){
+						$scope.submissions.push(res.data);
+						if (id == ids[ids.length-1]) { filterSubmissions() }
+					});
+				});
+			};
+
+			var getUserSubmissions = function() {
+				HNApi.showUser('whoishiring').then(function(res) {
+					var submissionIds = res.data.submitted.splice(0, submissionLimit);
+					getSubmissions(submissionIds);
+				});
+			};
 
 			$scope.postings = [];
 			$scope.filteredPostings = [];
@@ -14,8 +46,8 @@ function whoIsHiring(HNApi) {
 
 			$scope.searchQueries = [""];
 
-			var getPostings = function() {
-				$scope.story.kids.forEach(function(kidId){
+			$scope.getPostings = function(submission) {
+				submission.kids.forEach(function(kidId){
 					HNApi.showItem(kidId).then(function (res){
 						if(res.data.text) {
 							res.data.htmlText = $sce.trustAsHtml(res.data.text);
@@ -25,15 +57,7 @@ function whoIsHiring(HNApi) {
 				});
 			};
 
-			$scope.loadStory = function(){
-				if ($scope.storyId) {
-					HNApi.showItem($scope.storyId).then(function(res){
-						$scope.story = res.data;
-						getPostings();
-					})
-				}
-			};
-
+			getUserSubmissions();
 
   	}],
 		controllerAs: 'hiringCtrl',
